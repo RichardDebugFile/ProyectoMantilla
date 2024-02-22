496a99e7291cad34fe570da0d3fbbb3b28f78728
Ricardo <ricarhidalgo2020@gmail.com>
2024-02-21 20:10:02 -0500

    Noticas

---
+++ noticias.js
@@ -0,0 +1,21 @@
+// noticias.js
+var noticias = [
+    {
+      fecha: "25 Nov, 2014",
+      imagen: "assets/images/news1.jpg",
+      titulo: "Esto es una noticia",
+      descripcion: "En otras noticias, hola",
+      contenido: "Y así fue como el corredor más rápido terminó venciendo a su oponente quédese para más.",
+      enlace: "noticias.html"
+    },
+    // Agrega más objetos para otras noticias
+    {
+        fecha: "26 Nov, 2014",
+        imagen: "assets/images/news1.jpg",
+        titulo: "Título de la Noticia 2",
+        descripcion: "Lorem ipsum dolor sit amet conse ctetu eiusmod.",
+        contenido: "Contenido completo de la noticia 2. Lorem ipsum...",
+        enlace: "noticias.html"
+      },
+  ];
+  
